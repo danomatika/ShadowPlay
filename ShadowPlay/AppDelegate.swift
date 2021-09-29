24fcc3e6da37defb3a7172e53d6d5b1065965170
danomatika <danomatika@gmail.com>
2021-09-30 01:04:22 +0200

    fix missing Files app support

--- ShadowPlay/AppDelegate.swift
+++ ShadowPlay/AppDelegate.swift
@@ -46,6 +46,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 	}
 
+	/// don't open urls
+	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+		return false
+	}
+
 	// MARK: Util
 
 	/// returns full url to app resources pd directory
